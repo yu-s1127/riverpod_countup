d6dad10f316c2d2065a4a1bb002da5d9c88c67a7
yuhei_suzuki <yooopopo5@gmail.com>
2022-05-07 12:57:18 +0900

    UIの書き換え

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_coutup/provider.dart';
@@ -55,6 +56,32 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
               ref.watch(countProvider).toString(),
               style: Theme.of(context).textTheme.headline4,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: () => ref
+                      .read(countProvider.state)
+                      .update((state) => state + 1),
+                  tooltip: 'Increment',
+                  child: const Icon(CupertinoIcons.plus),
+                ),
+                FloatingActionButton(
+                  onPressed: () => ref
+                      .read(countProvider.state)
+                      .update((state) => state - 1),
+                  tooltip: 'Increment',
+                  child: const Icon(CupertinoIcons.minus),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text('1'),
+                Text('2'),
+              ],
+            ),
           ],
         ),
       ),
@@ -62,7 +89,7 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
         onPressed: () =>
             ref.read(countProvider.state).update((state) => state + 1),
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.refresh),
       ),
     );
   }
